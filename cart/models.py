4d1b79f7836d5d660c2dfc70c96af1de759be90a
Maksim Krylykov <maksimkrylykov@lawn-128-61-75-13.lawn.gatech.edu>
2025-09-18 17:43:40 -0400

    Multiple carts support

--- cart/models.py
+++ cart/models.py
@@ -8,8 +8,9 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
+    cart_number = models.PositiveSmallIntegerField(default=1, choices=[(1, 'Cart 1'), (2, 'Cart 2'), (3, 'Cart 3')])
     def __str__(self):
-        return str(self.id) + ' - ' + self.user.username
+        return f"{self.id} - {self.user.username} (Cart {self.cart_number})"
 
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
--- cart/views.py
+++ cart/views.py
@@ -6,42 +6,56 @@ from .models import Order, Item
 from django.contrib.auth.decorators import login_required
 
 def index(request):
+    cart_number = int(request.GET.get('cart_number', 1))
+    cart_key = f"cart_{cart_number}"
     cart_total = 0
     movies_in_cart = []
-    cart = request.session.get('cart', {})
+    cart = request.session.get(cart_key, {})
     movie_ids = list(cart.keys())
-    if (movie_ids != []):
+    if movie_ids:
         movies_in_cart = Movie.objects.filter(id__in=movie_ids)
         cart_total = calculate_cart_total(cart, movies_in_cart)
-    template_data = {}
-    template_data['title'] = 'Cart'
-    template_data['movies_in_cart'] = movies_in_cart
-    template_data['cart_total'] = cart_total
-    return render(request, 'cart/index.html',
-        {'template_data': template_data})
+    template_data = {
+        'title': f'Cart {cart_number}',
+        'movies_in_cart': movies_in_cart,
+        'cart_total': cart_total,
+        'cart_number': cart_number,
+        'cart': cart
+    }
+    return render(request, 'cart/index.html', {'template_data': template_data})
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def add(request, id):
     get_object_or_404(Movie, id=id)
-    cart = request.session.get('cart', {})
+    cart_number = int(request.POST.get('cart_number', 1))
+    cart_key = f"cart_{cart_number}"
+    cart = request.session.get(cart_key, {})
     cart[id] = request.POST['quantity']
-    request.session['cart'] = cart
-    return redirect('cart.index')
+    request.session[cart_key] = cart
+    return redirect(f'/cart/?cart_number={cart_number}')
 
 def clear(request):
-    request.session['cart'] = {}
-    return redirect('cart.index')
+    cart_number = int(request.POST.get('cart_number', 1))
+    cart_key = f"cart_{cart_number}"
+    request.session[cart_key] = {}
+    return redirect(f'/cart/?cart_number={cart_number}')
 
 @login_required
 def purchase(request):
-    cart = request.session.get('cart', {})
+    cart_number = int(request.POST.get('cart_number', 1))
+    cart_key = f"cart_{cart_number}"
+    cart = request.session.get(cart_key, {})
     movie_ids = list(cart.keys())
-    if (movie_ids == []):
-        return redirect('cart.index')
+    if not movie_ids:
+        return redirect(f'/cart/?cart_number={cart_number}')
     movies_in_cart = Movie.objects.filter(id__in=movie_ids)
     cart_total = calculate_cart_total(cart, movies_in_cart)
     order = Order()
     order.user = request.user
     order.total = cart_total
+    order.cart_number = cart_number
     order.save()
     for movie in movies_in_cart:
         item = Item()
@@ -50,9 +64,10 @@ def purchase(request):
         item.order = order
         item.quantity = cart[str(movie.id)]
         item.save()
-    request.session['cart'] = {}
-    template_data = {}
-    template_data['title'] = 'Purchase confirmation'
-    template_data['order_id'] = order.id
-    return render(request, 'cart/purchase.html',
-        {'template_data': template_data})
+    request.session[cart_key] = {}
+    template_data = {
+        'title': 'Purchase confirmation',
+        'order_id': order.id,
+        'cart_number': cart_number
+    }
+    return render(request, 'cart/purchase.html', {'template_data': template_data})
